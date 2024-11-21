f96eceab2735bd72bd850ad21ea0e96f8fe77115
Harsh Mehta <harsh_m@hotmail.com>
2024-11-20 21:56:34 -0500

    Added description for variable to show what variable represents

--- 2-input-variables/variables.tf
+++ 2-input-variables/variables.tf
@@ -1,7 +1,9 @@
 variable "policy_description" {
   type = string
+  description = "Description for policy string"
 }
 
 variable "policy_count" {
   type = number
+  description = "Number of policies to create"
 }
