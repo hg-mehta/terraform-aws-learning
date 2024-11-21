992e5eb449cd998d71d605e37956e81a84d63245
Harsh Mehta <harsh_m@hotmail.com>
2024-11-20 22:01:53 -0500

    Added variable default value

--- 2-input-variables/variables.tf
+++ 2-input-variables/variables.tf
@@ -1,9 +1,11 @@
 variable "policy_description" {
   type = string
   description = "Description for policy string"
+  default = "Description for policy_string"
 }
 
 variable "policy_count" {
   type = number
   description = "Number of policies to create"
+  default = 0
 }
